51156373c7e6e8de3f459bc24a51fb2214f1df90
satoshi <dev.satoshi.1016@gmail.com>
2023-09-24 07:28:02 +0900

    add: カスタムユーザーを作成

--- app/config/settings.py
+++ app/config/settings.py
@@ -28,8 +28,10 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     # サードパーティ
+    "allauth",
     # ローカル
     "medicine",
+    "accounts",
 ]
 
 MIDDLEWARE = [
@@ -124,3 +126,48 @@ MEDIA_ROOT = BASE_DIR / "mediafiles"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Authentication
+SITE_ID = 1
+# カスタムユーザーモデル
+AUTH_USER_MODEL = "accounts.CustomUser"
+
+# ユーザー認証に使用する方法を指定します
+ACCOUNT_AUTHENTICATION_METHOD = "email"
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_UNIQUE_EMAIL = True
+# サインアップする際にメールアドレスを2回入力してもらうかどうか(defaultはFalse)
+# ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = True
+# ユーザーのメールアドレス確認が必須であることを指定しています(mandatoryの場合は確認が必須、adminの場合は管理者が確認するまでアカウントが制限される)
+# ACCOUNT_EMAIL_VERIFICATION = "none"
+
+# ユーザーにパスワードを2回入力してもらうかどうか(defaultはTrue)
+# ACCOUNT_SIGNUP_PASSWORD_ENTER_TWICE = False
+
+# ユーザー名が必須かどうか
+ACCOUNT_USERNAME_REQUIRED = True
+# ユーザー名の許容される最小の長さを指定する整数
+ACCOUNT_USERNAME_MIN_LENGTH = 3
+
+LOGIN_REDIRECT_URL = "index"
+ACCOUNT_LOGOUT_REDIRECT_URL = "/accounts/login/"
+LOGIN_URL = "account_login"
+
+# GETリクエストでログアウトできるか指定
+ACCOUNT_LOGOUT_ON_GET = True
+
+# httpsを使用する場合
+# ACCOUNT_DEFAULT_HTTP_PROTOCOL = "https"
+
+
+# Email settings
+# コンソールでメールを確認
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+# 本番環境↓
+# EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+# EMAIL_HOST = "smtp.gmail.com"
+# EMAIL_PORT = 587
+# EMAIL_HOST_USER = "xxx@gmail.com"
+# EMAIL_HOST_PASSWORD = "xxx"
+# EMAIL_USE_TLS = True
+# DEFAULT_FROM_EMAIL = "xxx@gmail.com"
